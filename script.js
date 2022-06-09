5d3285f00ca6d6cd480893a587a87ecc7a90f193
enochrx <drreayy@gmail.com>
2022-06-09 20:07:28 +0100

    update

--- script.js
+++ script.js
@@ -3,5 +3,6 @@
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.check').addEventListener('.click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
 });
