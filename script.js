6ef067884b740c22d5d6bb229a3f3a7554272009
enochrx <drreayy@gmail.com>
2022-06-09 18:41:08 +0100

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.check').addEventListener('.click', function () {
+  console.log(document.querySelector('.guess').value);
+});
