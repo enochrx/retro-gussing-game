b2ef7a6682f6bc8b96f1311d6e5f743158e674c6
enochrx <drreayy@gmail.com>
2022-06-12 22:05:44 +0100

    functioning game

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 Math.random(); // Math is an object in JS and random() is one of the methods in the Math object. it will produce a decimal number btw 0 and 1, so in the context of this game, we need a number btw 0-20, so we have to multiply the method by 20(note: method = function in an object) and this is produce a decimer number btw 1 - 19.9999, and to get rid of this decimal, we need to introduce another Math method; Math.trunc(Math.random()) so we get a number btw 0 - 19, and will never really include a 20 there we need to add 1.
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 
 let score = 20; //Using let cos 'const' iks immutable and we new the value of score to decrease on kevery wrong guesses
 
@@ -22,8 +21,10 @@ document.querySelector('.check').addEventListener('click', function () {
     //When the guess is right and player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#5ddc36';
+    document.querySelector('.number').style.width = '30rem';
 
     //When guess is too Low
   } else if (guess < secretNumber) {
@@ -31,7 +32,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '😒 Guess too low!';
       score = score - 1; // also 'score--'.
       document.querySelector('.score').textContent = score;
-      document.querySelector('body').style.backgroundColor = '#f6dc1';
+      // document.querySelector('body').style.backgroundColor = '#f6dc1';
     } else {
       document.querySelector('.message').textContent = '🤷‍♂️ You lost the game!';
       document.querySelector('.score').textContent = 0;
@@ -44,7 +45,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '😂 Guess too high!';
       score--; // so score decreases by 1 on every wrong guess.
       document.querySelector('.score').textContent = score;
-      document.querySelector('body').style.backgroundColor = '#f6dc1';
+      // document.querySelector('body').style.backgroundColor = '#f6dc1';
     } else {
       document.querySelector('.message').textContent = '🤷‍♂️ You lost the game!';
       document.querySelector('.score').textContent = 0;
